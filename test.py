c922188d30c74315a70ca4f4a846a22057639dba
j-crit <j-crit@github.com>
2016-05-27 22:41:59 +1000

    experimental logger

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
-from sopel.module import commands
+from sopel.module import commands,rule,require_privmsg
+
+def setup(bot):
+    bot.memory["doAuto"] = True
 
 @commands('test')
 def test(bot,trigger):
-    bot.say(trigger.group(2))
+    bot.write(("version",),"centipeda")
+
+@commands('togglefeedback','tfb')
+def togglefd(bot,trigger):
+    bot.memory["doAuto"] = not bot.memory["doAuto"]
+    bot.say("Feedback set to " + str(bot.memory["doAuto"]))
+    
+
+@rule('.*')
+def auto(bot,trigger):
+    if bot.memory["doAuto"]:
+        line = trigger.sender + ": " + trigger.nick + ": " + trigger.group(0)
+        bot.say(line,"#criticalstrike")
+
